30c95c19c599180d7bee0e13dd48cb0a6f1cb2ad
Macfe1 <mac1515.pp@gmail.com>
2024-06-26 00:15:50 -0500

    Adding all

--- aux_func.c
+++ aux_func.c
@@ -11,50 +11,43 @@
  */
 int aux_funct(const char *format, va_list param_to_exe, char *buffer)
 {
-	int counter = 0, counter_arg = 0, validOperation = 0, size = 0;
+	int counter = 0, con_arg = 0, validOperation = 0, size = 0;
 	convert_arg funct_struct[] = {
 
 		{'c', pr_char},
 		{'s', pr_string},
 		{'\0', NULL}
 	};
-	
+
 	if (format == NULL)
 		return (-1);
-
 	for (counter = 0; format[counter] != '\0'; counter++)
 	{
 		if (format[counter] != '%')
 		{
-			buffer[size] = format[counter];
-			size++;
+			buffer[size++] = format[counter];
 			continue;
 		}
-		
-		counter_arg = 0;
-		validOperation = 0;
-
-		while (funct_struct[counter_arg].type != '\0')
+		if (format[counter + 1] == '%')
 		{
-			if (format[counter + 1] == '\0')
+			buffer[size++] = '%';
+			counter++;
+			continue;
+		}
+		validOperation = 0;
+		for (con_arg = 0; funct_struct[con_arg].type != '\0'; con_arg++)
+		{
+			if (format[counter + 1] == funct_struct[con_arg].type)
 			{
-				validOperation = 0;
-				break;
-			}
-			if (format[counter + 1] == funct_struct[counter_arg].type)
-			{
-				size += funct_struct[counter_arg].p_function(param_to_exe, size, buffer);
+				size += funct_struct[con_arg].p_function(param_to_exe, size, buffer);
 				counter++;
 				validOperation = 1;
 				break;
 			}
-			validOperation = 0;
-			counter_arg++;
 		}
 		if (!validOperation)
 		{
-			buffer[size] = format[counter];
-			size++;
+			buffer[size++] = format[counter];
 		}
 	}
 	return (size);
